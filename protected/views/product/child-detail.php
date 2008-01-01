b958821e2e4cacaf1bcd07b3f33a70b0dbb63c5f
AnhDT <dotheanh@kgoin.com>
2008-01-01 11:52:39 +0700

    style and complete product-page

--- protected/views/product/child-detail.php
+++ protected/views/product/child-detail.php
@@ -1,4 +1,30 @@
 <?php 
+
+$categoriesMenuItems = array();
+
+$categories = Categories::model()->findAll();
+
+foreach($categories as $_category) {
+	$categoriesMenuItems[] = array('label'=>$_category->category_alias, 'url'=>array('product/list', 'category'=>$_category->category_id));
+}
+
+Yii::app()->getClientScript()->registerCssFile(Yii::app()->request->baseUrl.'/css/jquery.lightbox-0.5.css');
+Yii::app()->getClientScript()->registerCssFile(Yii::app()->request->baseUrl.'/css/bootstrap.css');
+Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl.'/js/bootstrap.js');
+Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . '/js/jquery.lightbox-0.5.js');
+$images = $product->product_picture;
+if(is_array($images)) {
+	$mainImage = array_shift($images);
+	$mainImage = is_object($mainImage)?$mainImage->image_file:$mainImage;
+}else if(is_string($images)){
+	$mainImage = $images;
+	$images = array();
+}else{
+	$mainImage = Product::DEFAULT_IMAGE;
+	$images = array();
+}
+
+
 $parentId = $product->parent_id;
 $childProducts = Product::model()->findAll(array(
 	'condition'=> 'parent_id = :parentId ',
@@ -22,7 +48,7 @@ endforeach;
 		<div class="col-right">
 			<ul class="product-important-information">
 				<li class="product-image">
-				<img src="<?php echo Yii::app()->request->baseUrl; ?>/<?php $product->product_picture; ?>" />
+				<img src="<?php echo Yii::app()->request->baseUrl; ?>/<?php echo $product->product_picture; ?>" />
 				</li>
 				<li class="product-information">
 		<div class="title"><h1><?php echo $product->product_name?></h1></div>				
