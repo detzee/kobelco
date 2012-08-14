31e915ad09550cca3d3064ac84b3af268d4c7798
Do The Anh <dz.walkaz@gmail.com>
2012-08-15 02:53:13 +0700

    add list product content

--- protected/views/product/list.php
+++ protected/views/product/list.php
@@ -19,7 +19,7 @@ if(!$categoryId) {
 
 $category = Categories::model()->findByPk($categoryId);
 
-$products = Product::model()->findAll();			
+$products = Product::model()->findAll("category_id=:categoryId", array(":categoryId" => $categoryId));
 
 $this->breadcrumbs=array(
 	'Products'=>$productIndex,
@@ -27,7 +27,7 @@ $this->breadcrumbs=array(
 );
 
 ?>
-<div class="product-detail-content">
+<div class="product-list-wrapper">
 	<div class="col-left">
 			<div class="categories">
 				<div class="title" ><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/Product-select-section_03.png" /></div>
@@ -45,5 +45,17 @@ $this->breadcrumbs=array(
 					</ul>
 				</div>
 			</div>
+	</div>
+	<div class="product-list-content">
+		<div class="product-selection"><?php echo $category->category_alias?></div>
+		<ul class="list-product">
+			<?php foreach($products as $product) :?>
+			<li>
+				<div class="images-holder"><img src="images/Product_item1.png" /></div>
+				<p>22-75kW Class<br/>Discharge air volume:<br/>4.18-15.0m³/min(0.8-0.6MPa)</p>
+				<div class="detail"><?php echo CHtml::link('Detail', array('product/detail', 'product' => $product->product_id))?></div>
+			</li>
+			<?php endforeach; ?>
+		</ul> <!-- list-product -->
 	</div>
 </div>
