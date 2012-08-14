9bb6902fa133a2f295001a8834b8f127076d7157
Do The Anh <dz.walkaz@gmail.com>
2012-08-15 02:29:50 +0700

    add filter on left sidebar

--- protected/views/product/list.php
+++ protected/views/product/list.php
@@ -29,9 +29,21 @@ $this->breadcrumbs=array(
 ?>
 <div class="product-detail-content">
 	<div class="col-left">
-			<div class="menu">
+			<div class="categories">
 				<div class="title" ><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/Product-select-section_03.png" /></div>
 				<?php $this->widget('zii.widgets.CMenu',array('items'=> $categoriesMenuItems)); ?>
+			</div>
+			<div class="filter_product">
+				<div class="title"><img src="<?php echo Yii::app()->request->baseUrl;?>/images/Product-select-section_16.png" /></div>
+		
+				<div class="power">
+					<span>Power:</span>
+					<ul class="Power:">
+						<li><p>22 kW - 75 kW</p></li>
+						<li><p>75 kW - 100 kW</p></li>
+						<li><p>100 kW - 200 kW</p></li>
+					</ul>
+				</div>
 			</div>
 	</div>
 </div>
