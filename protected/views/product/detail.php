2b5292c70558a6c697f6fe0997644591caf81edb
Do Ha Son <sondoha@gmail.com>
2012-08-19 03:03:00 +0700

    style for contact page and add lightbox jquery

--- protected/views/product/detail.php
+++ protected/views/product/detail.php
@@ -7,11 +7,10 @@ foreach($categories as $_category) {
 	$categoriesMenuItems[] = array('label'=>$_category->category_alias, 'url'=>array('product/list', 'category'=>$_category->category_id));
 }
 
-
-
-
+Yii::app()->getClientScript()->registerCssFile(Yii::app()->request->baseUrl.'/css/jquery.lightbox-0.5.css');
 Yii::app()->getClientScript()->registerCssFile(Yii::app()->request->baseUrl.'/css/bootstrap.css');
 Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl.'/js/bootstrap.js');
+Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . '/js/jquery.lightbox-0.5.js');
 $images = $product->product_picture;
 $mainImage = array_shift($images);
 $mainImage = is_object($mainImage)?$mainImage->image_file:Product::DEFAULT_IMAGE;
@@ -40,7 +39,11 @@ $mainImage = is_object($mainImage)?$mainImage->image_file:Product::DEFAULT_IMAGE
 			<div class="list-thumbs">
 				<ul class="list-thumb">
 					<?php foreach($images as $image):?>
-					<li class="item"><img src="<?php echo $image->image_file?>" /></li>
+					<li class="item">
+						<a href="<?php echo Yii::app()->request->baseUrl ?>/images/Product-detail_10.png">
+						<img src="<?php echo $image->image_file?>" />
+						</a>
+					</li>
 					<?php endforeach?>
 				</ul>
 			</div>
@@ -56,3 +59,11 @@ $mainImage = is_object($mainImage)?$mainImage->image_file:Product::DEFAULT_IMAGE
 
 </div><!-- #product-detail-content -->
 
+
+<script type="text/javascript">
+$(function() {
+	// Use this example, or...
+	$('.list-thumb a').lightBox(); // Select all links that contains lightbox in the attribute rel
+
+});
+</script>
