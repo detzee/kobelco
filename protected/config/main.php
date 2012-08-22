6d886c55dbeda2b068ed148c17d43313a4ea4c9d
AnhDT <dotheanh@kgoin.com>
2012-08-23 01:27:04 +0700

    fixed style of news and main-menu on home page and fixed style in IE7,8

--- protected/config/main.php
+++ protected/config/main.php
@@ -68,7 +68,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=kobelco',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => 'root',
+			'password' => '',
 			'charset' => 'utf8',
 		),
 		
