fdbfc89fcabd426f5374159764ddfc153e12f093
AnhDT <dotheanh@kgoin.com>
2012-10-07 11:02:00 +0700

    zzz

--- protected/messages/vi/site.php
+++ protected/messages/vi/site.php
@@ -1,4 +1,5 @@
 <?php
+
 return array(
 	'News' => 'Tin tức',
 	'Contact' => 'Liên hệ',
@@ -15,11 +16,19 @@ return array(
 	'Search'=>'Tìm kiếm',
 	'Lastest news'=>'Tin mới nhất',
 	'Contact us'=>'Liên hệ với chúng tôi',
-	'If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.'=>'Nếu bạn có yêu cầu kinh doanh hoặc các câu hỏi khác, xin vui lòng điền vào mẫu dưới đây để liên hệ với chúng tôi. Cảm ơn bạn.',
+	'If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.'=>'Nếu bạn có yêu cầu kinh doanh hoặc các câu hỏi khác, xin vui lòng điền vào mẫu đơn sau đây để liên hệ với chúng tôi. Cảm ơn bạn!',
 	'Please enter the letters as they are shown in the image above. Letters are not case-sensitive.'=>'Xin vui lòng nhập các ký tự như được hiển thị trong hình trên. Các chữ cái không phân biệt hoa thường.',
-	'Fields with'."<span style='color: red;'> *</span>".' are required.'=>'Các trường có dấu'."<span style='color: red;'> *</span>".' là bắt buộc',
+	'Fields with'."<span style='color: red;'> *</span>".' are required.'=>'Các trường có dấu'."<span style='color: red;'> *</span>".'là bắt buộc',
 	'Name'=>'Tên',
 	'Subject'=>'Tiêu đề',
 	'Body'=>'Nội dung',
 	'Verification Code'=>'Mã xác nhận',
+	'Kobelco is a group of multidisciplinary industrial manufacturing multinationals with revenues of over 20 billion dollars per year, which was founded in 1905 in Tokyo, Japan, with three main business segments: iron and steel production,aluminum, and mechanical engineering. In the field of mechanical engineering, production and sales department compressor dominant position.'=> 'Kobelco là một  tập đoàn sản xuất công nghiệp đa ngành nghề đa quốc gia với doanh thu trung bình hơn 20 tỷ đô la một năm, được thành lập năm 1905 tại Tokyo Nhật bản, với 3 mảng kinh doanh chính: Sản xuất sắt thép,  Nhôm đồng, và cơ khí chế tạo. Trong lĩnh vục cơ khí chế tạo, bộ phận sản xuất và kinh doanh máy nén khí chiếm vị trí chủ đạo.',
+	'About Kobelco Group'=>'Giới thiệu tập đoàn Kobelco',
+	'Kobelco\'s History'=>'Lịch sử phát triển của tập đoàn Kobelco',
+	'About Kobelco Compressor VietNam'=>'Giới thiệu về công ty Kobelco Compressor ViệtNam',
+	'Kobelco\'s Target'=>'Mục tiêu của công ty',
+	'Kobelco\'s Structure'=>'Cơ cấu tổ chức',
 );
+
+?>
